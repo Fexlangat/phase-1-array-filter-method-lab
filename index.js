b26ca3b96fd90e8854d23bbd84341905c1fa639e
Felix Kipkurui Langat <fexlangat@gmail.com>
2024-03-25 18:47:35 +0300

    array filters

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+
+drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, name) {
+
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  findMatching;
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  drivers;
+
+   driversData = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  
+  module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName
+  };
